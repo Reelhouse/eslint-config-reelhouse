9a3b31860bb0fb0173eb1c46850c1274c1b4a753
Aaron Franks <aaron.franks@gmail.com>
2017-08-30 11:01:09 -0700

    Add spaced-comment exception

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ module.exports = {
         'comma-dangle': OFF,
         'max-len': [WARN, 100, 4, {'ignoreUrls': true, 'ignoreComments': true}],
         'no-warning-comments': WARN,
-        'require-yield': OFF,       // Doesn't play nice with simple Koa routes
+        'require-yield': OFF,       // Doesn't play nice with simple Koa 1.x routes
+        'spaced-comment': ['error', 'always', { 'exceptions': [':'] }],     // For flow comments like /*: foo */
 
         // Further ES6 enhancements
         // Any rules here set to OFF/WARN are things to turn on eventually
